9bd51316b011f432ad35ab1d292ccc4abbe6f395
Illia Dolzhenko <Illia_Dolzhenko@epam.com>
2021-02-04 16:30:46 +0200

    FOLREL-473 Project Setup

---
+++ src/test/java/org/folio/modulesdependencymatrixapp/ModulesDependencyMatrixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.folio.modulesdependencymatrixapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesDependencyMatrixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
